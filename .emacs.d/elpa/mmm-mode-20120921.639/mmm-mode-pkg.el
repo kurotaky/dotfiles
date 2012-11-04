794500dc67a49c2563e5cc24c858d293a8c1ce3a
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2012-11-04 23:19:38 +0900

    Add mmm-mode

---
+++ .emacs.d/elpa/mmm-mode-20120921.639/mmm-mode-pkg.el
@@ -0,0 +1,2 @@
+(define-package "mmm-mode" "20120921.639" "Allow Multiple Major Modes in a buffer [source: github]" 'nil)
+
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,6 +59,34 @@
 ;; php-mode
 (require 'php-mode)
 
+;; mmm-mode
+(require 'mmm-mode)
+; (require 'mmm-auto)
+(setq mmm-submode-decoration-level 2)
+(invert-face 'mmm-default-submode-face t)
+(setq mmm-font-lock-available-p t)
+(setq mmm-global-mode 'maybe)
+; (set-face-bold-p 'mmm-default-submode-face t)
+; (set-face-background 'mmm-default-submode-face nil)
+(mmm-add-mode-ext-class nil "\\.php?\\'" 'html-php)
+(mmm-add-classes
+  '((html-php
+         :submode php-mode
+             :front "<\\?\\(php\\)?"
+                 :back "\\?>")))
+(add-to-list 'auto-mode-alist '("\\.php?\\'" . html-mode))
+
+
+;; タブの改善
+(defun save-mmm-c-locals ()
+    (with-temp-buffer
+          (php-mode)
+              (dolist (v (buffer-local-variables))
+                      (when (string-match "\\`c-" (symbol-name (car v)))
+                                (add-to-list 'mmm-save-local-variables `(,(car v) nil, mmm-c-derived-modes))))))
+(save-mmm-c-locals)
+
+
 ;;; 適用する拡張子
 (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
