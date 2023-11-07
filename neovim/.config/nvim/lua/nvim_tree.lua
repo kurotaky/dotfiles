4353791ca54fb138f48beeb467b21821fe0567bc
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2023-11-07 16:59:00 +0900

    view.mappings.listはdeprecatedので削除

--- neovim/.config/nvim/lua/nvim_tree.lua
+++ neovim/.config/nvim/lua/nvim_tree.lua
@@ -1,22 +1,23 @@
-require('nvim-tree').setup({
-  sort_by = 'case_sensitive',
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = 'u', action = 'dir_up' },
-      },
-    },
-    side = 'right',
-  },
-  renderer = {
-    group_empty = true,
-    highlight_git = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
 
--- start neovim with open nvim-tree
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+end
+
+-- pass to setup along with your other options
+require("nvim-tree").setup {
+  ---
+  on_attach = my_on_attach,
+  ---
+}
+
 require("nvim-tree.api").tree.toggle(false, true)
